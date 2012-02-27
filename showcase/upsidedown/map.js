f0cb8d4aaedbed3b4ef8f9ffd198d3302eeb4117
Gregor Aisch <contact@vis4.net>
2012-02-27 02:09:37 +0100

    reworked filenames

--- showcase/upsidedown/map.js
+++ showcase/upsidedown/map.js
@@ -3,7 +3,7 @@ $(function() {
 
 	var map = $K.map('#map');
 
-	map.loadMap('upsidedown.svg', function() {
+	map.loadMap('map-upsidedown.svg', function() {
 		map.addLayer('sea');
 		map.addLayer('graticule');
 		map.addLayer('countries');
